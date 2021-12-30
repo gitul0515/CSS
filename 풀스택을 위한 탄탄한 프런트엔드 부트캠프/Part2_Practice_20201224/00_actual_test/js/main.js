883f8fbbdf2e656e6cd25dc528b052ba185ee394
gitul0515 <gitul0515@gmail.com>
2021-12-30 21:49:18 +0900

    잔재미코딩 웹사이트 HTML/CSS 수정

--- 풀스택을 위한 탄탄한 프런트엔드 부트캠프/Part2_Practice_20201224/00_actual_test/js/main.js
+++ 풀스택을 위한 탄탄한 프런트엔드 부트캠프/Part2_Practice_20201224/00_actual_test/js/main.js
@@ -1,15 +1,35 @@
+// backToTop 버튼
 const backToTop = document.getElementById('backtotop');
 
-window.addEventListener('scroll', () => {
-  const scrollYPos = window.scrollY;
-
-  if (scrollYPos !== 0) {
+function scrollCheck() {
+  if (scrollY !== 0) {
     backToTop.classList.add('show');
   } else {
     backToTop.classList.remove('show');
   }
-});
+}
 
-backToTop.addEventListener('click', () => {
+function moveBackToTop() {
   scrollTo({top: 0, behavior:'smooth'});
-});
+}
+
+window.addEventListener('scroll', scrollCheck);
+backToTop.addEventListener('click', moveBackToTop);
+
+// class-list 이동 버튼
+const _arrow = document.querySelectorAll('.slide-prev');
+const arrow = [..._arrow];
+
+for(let i = 0; arrow.length; i++) {
+  const ul = arrow[i].parentNode.parentNode.nextElementSibling;
+  const liList = ul.children;
+
+  if (ul.clientWidth < liList.length * 260) {
+    arrow[i].classList.add('hover');
+    // arrow[i].addEventListener('click', movement);
+  } else {
+    const parent = arrow[i].parentNode;
+    parent.removeChild(arrow[i].nextElementSibling);
+    parent.removeChild(arrow[i]);
+  }
+}
