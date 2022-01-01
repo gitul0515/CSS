e00a63d8fc238a8e68bc927c3d810a6cac6407c6
gitul0515 <gitul0515@gmail.com>
2022-01-01 15:03:49 +0900

    탭 콘텐츠 슬라이더 구현

--- 풀스택을 위한 탄탄한 프런트엔드 부트캠프/Part2_Practice_20201224/00_actual_test/js/main.js
+++ 풀스택을 위한 탄탄한 프런트엔드 부트캠프/Part2_Practice_20201224/00_actual_test/js/main.js
@@ -27,6 +27,7 @@ for (let i = 0; i < prevArrows.length; i++) {
   if (ul.clientWidth < lis.length * 260) {
     // 왼쪽 버튼 활성화
     prevArrows[i].addEventListener('click', moveLeft);
+    prevArrows[i].classList.add('hover');
   } else {
     // 버튼을 모두 삭제한다
     const parent = prevArrows[i].parentNode;
@@ -51,12 +52,14 @@ function moveLeft(event) {
   // 오른쪽 버튼을 활성화
   nextArrow.style.color = 'rgb(47, 48, 89)';
   nextArrow.addEventListener('click', moveRight);
+  nextArrow.classList.add('hover');
 
   // 오른쪽에 가려진 콘텐츠가 없으면
   if (ul.clientWidth >= lis.length * 260 + position) {
     // 왼쪽 버튼을 비활성화
     prevArrow.style.color = '#cfd8dc';
     prevArrow.removeEventListener('click', moveLeft);
+    prevArrow.classList.remove('hover');
   }
 
   // data-position 값을 업데이트
@@ -74,6 +77,7 @@ function moveRight(event) {
   // 왼쪽 버튼을 활성화
   prevArrow.style.color = 'rgb(47, 48, 89)';
   prevArrow.addEventListener('click', moveLeft);
+  prevArrow.classList.add('hover');
   
   // ul 콘텐츠를 오른쪽으로 이동
   ul.style.transitionProperty = 'transform';
@@ -85,6 +89,7 @@ function moveRight(event) {
     // 오른쪽 버튼을 비활성화
     nextArrow.style.color = '#cfd8dc';
     nextArrow.removeEventListener('click', moveRight);
+    nextArrow.classList.remove('hover');
   }
   
   // data-position 값을 갱신
