a04315f0b2954f8cd2b8eb3c72d0e2d12bbd0f63
gitul0515 <gitul0515@gmail.com>
2022-02-09 14:36:14 +0900

    project filtering 기본 구현

--- 실전퍼블리싱/JS/22_main.js
+++ 실전퍼블리싱/JS/22_main.js
@@ -1,13 +1,23 @@
 const categoryBtns = document.querySelector('.projects__categories');
 let curBtn = categoryBtns.firstElementChild;
-console.log(curBtn);
+const projects = [...document.querySelector('.projects__container').children];
+console.log(projects);
 
 categoryBtns.addEventListener('click', e => {
   const target = e.target;
-  const filter = target.dataset.filter;
-  if (!filter) return;
+  const btnFilter = target.dataset.filter;
+  if (!btnFilter) return;
   
   curBtn.classList.remove('checked');
   curBtn = target;
   curBtn.classList.add('checked');
+
+  projects.forEach(project => {
+    const projectFilter = project.dataset.filter;
+    if (projectFilter === btnFilter || btnFilter === 'all') {
+      project.classList.add('show');
+    } else {
+      project.classList.remove('show');
+    }
+  });
 })
